fa4b9b694c124a92b278201c06926b78dfe64539
haifapremium <47364180+haifapremium@users.noreply.github.com>
2019-02-07 18:58:05 -0200

    Arquivo inicial
    Add novos arquivo iniciais

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php include("haifa/mvcHaifa/core/database/teste.class.php"); ?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="utf-8">
+	<title>API :: Haifa premium</title>
+	<meta name="copyright" content="(c) 2019 ">
+	<meta description="Desenvolvimento da API de intergração ERP">
+</head>
+<body>
+ 
+ <?php
+ 
+ 		$testando = new teste();	
+ 	
+ ?>
+
+</body>
+</html>
